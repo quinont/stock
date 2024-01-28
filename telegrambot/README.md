91fd895f4e664ba3aff3fbfe2ea9f9033c000448
Juan Esteban Colcombet <quinont@gmail.com>
2024-01-28 18:08:46 -0300

    Agregando un bot basico de telegram que escucha un topico de mosquitto

---
+++ telegrambot/README.md
@@ -0,0 +1,3 @@
+# Bot basico
+
+Este es un bot basico que escucha un topico de mosquitto y escribe en un canal telegram.
---
+++ telegrambot/main.py
@@ -0,0 +1,59 @@
+import paho.mqtt.client as mqtt
+from telegram import Bot
+from telegram import Update
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
+import os
+
+# Configuración para el bot de Telegram
+TELEGRAM_BOT_TOKEN = os.environ.get("TELEGRAM_BOT_TOKEN")
+TELEGRAM_CHAT_ID = os.environ.get("TELEGRAM_CHAT_ID")
+
+# Configuración para el servidor Mosquitto
+MQTT_BROKER_ADDRESS = os.environ.get("MQTT_BROKER_ADDRESS")
+MQTT_TOPIC = os.environ.get("MQTT_TOPIC", "#")
+
+
+def on_message(client, userdata, msg):
+    message = msg.payload.decode('utf-8')
+    queue_name = msg.topic
+    send_telegram_message(f"Nuevo mensaje de Mosquitto en la cola {queue_name}:\n\n{message}")
+
+
+def send_telegram_message(message):
+    bot = Bot(token=TELEGRAM_BOT_TOKEN)
+    bot.send_message(chat_id=TELEGRAM_CHAT_ID, text=message)
+
+
+def start(update: Update, context: CallbackContext):
+    update.message.reply_text("¡Bot de Mosquitto y Telegram iniciado!")
+
+
+def main():
+    # Configuración del cliente MQTT
+    mqtt_client = mqtt.Client()
+    mqtt_client.on_message = on_message
+    mqtt_client.connect(MQTT_BROKER_ADDRESS, 1883, 60)
+    mqtt_client.subscribe(MQTT_TOPIC)
+    mqtt_client.loop_start()
+
+    # Configuración del bot de Telegram
+    updater = Updater(token=TELEGRAM_BOT_TOKEN)
+    dispatcher = updater.dispatcher
+
+    # Manejadores de comandos
+    start_handler = CommandHandler('start', start)
+    dispatcher.add_handler(start_handler)
+
+    # Manejador de mensajes
+    message_handler = MessageHandler(Filters.text & ~Filters.command, start)
+    dispatcher.add_handler(message_handler)
+
+    # Inicia el bot de Telegram
+    updater.start_polling()
+
+    # Mantén el programa en ejecución
+    updater.idle()
+
+
+if __name__ == "__main__":
+    main()
